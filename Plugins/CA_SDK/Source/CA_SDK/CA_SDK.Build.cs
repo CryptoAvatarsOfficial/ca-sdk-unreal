183429f87891af071419e2a5a52c77c7d83d556c
notbloodmask <114909420+notbloodmask@users.noreply.github.com>
2023-01-19 14:35:38 +0100

    WIP - Avatar VRM Loading done using VRM4ULoader but needs further testing.

--- Plugins/CA_SDK/Source/CA_SDK/CA_SDK.Build.cs
+++ Plugins/CA_SDK/Source/CA_SDK/CA_SDK.Build.cs
@@ -32,7 +32,8 @@ public class CA_SDK : ModuleRules
 				"JsonUtilities",
                 "UMG",
 				"Slate",
-				"SlateCore"
+				"SlateCore",
+                "VRM4ULoader",
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
