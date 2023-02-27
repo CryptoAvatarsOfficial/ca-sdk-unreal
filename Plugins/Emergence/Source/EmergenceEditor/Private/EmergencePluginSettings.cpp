655e2b24cfe1d246259c6c6a2824af4c73cd1be7
notbloodmask <114909420+notbloodmask@users.noreply.github.com>
2023-02-27 17:04:49 +0100

    Added third party libraries

--- Plugins/Emergence/Source/EmergenceEditor/Private/EmergencePluginSettings.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Crucible Networks Ltd 2023. All Rights Reserved.
-
-#include "EmergencePluginSettings.h"
-
-UEmergencePluginSettings::UEmergencePluginSettings(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-
-}
