12a39509356dccbd908363f90dcec341b1b14cf9
notbloodmask <114909420+notbloodmask@users.noreply.github.com>
2023-02-27 10:24:36 +0100

    Added Plugins folder Fully to avoid missing files

--- Source/ca_unreal_sdk/ca_unreal_sdk.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ca_unreal_sdk.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ca_unreal_sdk, "ca_unreal_sdk" );
- 
